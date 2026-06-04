28aadd9782c41787c2473c5f96c9b0f8fc6a30b8
jyeon3883 <jumbooo@naver.com>
2026-06-04 16:53:48 +0900

    경로수정

--- apps/web/src/application/providers/AppProviders.tsx
+++ apps/web/src/application/providers/AppProviders.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v15-appRouter";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v16-appRouter";
 import { QueryProvider } from "@repo/query/provider";
 import { UiThemeProvider } from "@repo/ui";
 
